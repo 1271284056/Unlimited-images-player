76e9162d5312a1b0d8f5b4cf556f2c79b182343f
JiangDong Zhang <1271284056@qq.com>
2017-07-20 13:50:06 +0800

    Exert

--- 无限图片轮播器/无限图片轮播器/AppDelegate.m
+++ 无限图片轮播器/无限图片轮播器/AppDelegate.m
@@ -1,7 +1,6 @@
 //
 //  AppDelegate.m
 //  无限图片轮播器
-//
 //  Created by 张江东 on 16/10/25.
 //  Copyright © 2016年 58kuaipai. All rights reserved.
 //
@@ -21,7 +20,6 @@
 
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
 }
 
